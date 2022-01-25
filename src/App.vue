4dc950ae5884e642f9bbdc5f0db2b832cd323107
Breezy-yarus <bricetchiaga@gmail.com>
2022-01-25 20:59:08 +0100

    Created a login-component with vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,51 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<!-- Beautifull accessible cross module login component -->
+
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >    </v-app-bar>
+
+    <v-content>
+      <v-card width="500" class="mx-auto mt-5">
+        <v-card-title>
+          <h1 class="display-1">Login</h1>
+        </v-card-title>
+        <v-card-text > 
+          <v-form>
+          <v-text-field prepend-icon="mdi-account" type="text" label="Username"></v-text-field>
+          <v-text-field prepend-icon="mdi-lock" :type="showPassword ? 'text': 'password'" label="Password" :append-icon="showPassword ? 'mdi-eye':'mdi-eye-off'" @click:append="showPassword= !showPassword"></v-text-field>
+</v-form>
+        </v-card-text>
+        <v-divider></v-divider>
+        <v-card-actions>
+          <v-btn color="success">Register</v-btn>
+        <v-spacer></v-spacer>
+          <v-btn color="primary">Login</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-content>
+
+    <v-main>
+  
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  components: {
+  
+  },
+
+  data: () => ({
+    showPassword:false
+  }),
+};
+</script>
